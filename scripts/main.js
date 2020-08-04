c0ef89220598ccb2c2d52283ed5be23b0d160c4d
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-08-04 17:39:39 +0100

    Test #7

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,36 @@
 const {Component} = React
 
-$(function () {
+class Test extends Component {
+	render() {
+		return (
+			<div className="jumbotron bg-secondary text-dark">
+				<div className="container">
+					<h4>Freaking sweet</h4>
+				</div>
+			</div>
+		);
+	}
+}
 
-	ReactDOM.render(<Demo/>, $("div#main-react")[0])
+let OLD_VIEW = "card"
+
+$(function () {
 
 	$("button#main-btn").click(function () {
 		if (window.confirm("Remove all cards from the UI ?")) {
 			$("div#main-btn").children();
 		}
 	})
+
+	$("button#toggle-btn").click(function () {
+		if (OLD_VIEW == "card") {
+			ReactDOM.render(<Demo/>, $("div#main-react")[0])
+			OLD_VIEW = "jumbotron"
+		} else {
+			ReactDOM.render(<Test/>, $("div#main-react")[0])
+			OLD_VIEW = "card"
+		}
+	}).trigger("click");
 
 	$("button#about-btn").on("click", function () {
 		window.alert("This is the about page");
